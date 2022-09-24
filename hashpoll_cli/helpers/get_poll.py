8c030662bf265504fc16e16d0dab515c06a098a4
Arpan Pandey <arpan@hackersreboot.tech>
2022-09-24 12:40:50 +0530

    Update URL

--- hashpoll_cli/helpers/get_poll.py
+++ hashpoll_cli/helpers/get_poll.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_poll_help(poll_id: str):
-    response = requests.get(f"https://24yl4zi1hh.execute-api.us-east-1.amazonaws.com/get/{poll_id}")
+    response = requests.get(f"https://hashapi.hackersreboot.tech/get/{poll_id}")
     if response.status_code == 200:
         return response.json()["poll"]
     return False
--- hashpoll_cli/helpers/vote.py
+++ hashpoll_cli/helpers/vote.py
@@ -4,7 +4,7 @@ def vote_help(poll_id: str, option: int):
     if option not in [1, 2, 3, 4]:
         raise ValueError("Option must be 1, 2, 3, 4")
     data_to_send = {"poll_id": poll_id, "vote": option}
-    response = requests.post("https://24yl4zi1hh.execute-api.us-east-1.amazonaws.com/vote", json=data_to_send)
+    response = requests.post("https://hashapi.hackersreboot.tech/vote", json=data_to_send)
     if response.status_code == 200:
         return True
     return False
